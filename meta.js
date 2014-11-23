25fef88c295af8c5f85a49876da339229ee252be
ccieivan <ccieivan@gmail.com>
2014-11-23 09:42:58 +0800

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,14 +1,13 @@
 {
-    "site_name": "小天天博客",
-    "copyright": "wawasoft 2012",
+    "site_name": "ccieivan",
+    "copyright": "ccieivan 2014",
     "cates": [
-        {"name": "tech", "text":"技术"},
-        {"name": "art", "text":"艺术"},
-        {"name": "other", "text":"其它"}
+        {"name": "tech", "text":"Harvard"},
+        {"name": "art", "text":"Coursera"},
+        {"name": "other", "text":"Others"}
     ],
     "articles":[
         {"title": "tech hello world", "file": "2012-01-23", "cate":"tech"},
         {"title": "art hello world", "file": "2012-02-23", "cate":"art"},
-        {"title": "other hello world", "file": "2012-03-23", "cate":"other"}
         ]
 }
