fd11f3387d69899608bce4f0fa6efed0f9068609
ccieivan <ccieivan@gmail.com>
2014-11-23 10:16:53 +0800

    Update meta.js

--- js/meta.js
+++ js/meta.js
@@ -6,7 +6,8 @@
         {"name": "art", "text":"Coursera"}
     ],
     "articles":[
-        {"title": "tech hello world", "file": "2012-01-23", "cate":"tech"},
-        {"title": "art hello world", "file": "2012-02-23", "cate":"art"}
+        {"title": "Harvard", "file": "Harvard", "cate":"art"},
+        {"title": "tech hello world", "file": "2012-01-23", "cate":"tech"}
+
     ]
 }
